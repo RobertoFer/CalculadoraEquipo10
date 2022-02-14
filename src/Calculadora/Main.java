7d049f47630256a71a94a16024559877972ef09e
Roberto Fernandez <robertofernandez110@gmail.com>
2022-02-14 22:37:32 +0100

    Inicio2

--- src/Calculadora/Main.java
+++ src/Calculadora/Main.java
@@ -23,21 +23,22 @@ public class Main {
 		switch(te) {
 		case '+':
 			System.out.println("///////////Suma///////////////");
-			Scanner num = new Scanner(System.in);
-			String num1 = num.next();
 
 			break;
+			
 		case '-':
 			System.out.println("///////////Resta///////////");
-			System.out.println("Puedes restar hastra 3 numeros");
+			
 			break;
+			
 		case '*':
 			System.out.println("///////////Multiplicacion///////////");
-			System.out.println("Puedes multiplicar hastra 3 numeros");
+
 			break;
+			
 		case '/':
 			System.out.println("///////////Division///////////");
-			System.out.println("Puedes dividir hastra 2 numeros");
+
 			break;
 		}
 		
